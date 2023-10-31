f8205d72f68868563ba2923c64bf87ad92589aa5
Shinichi <s1930557@s.tsukuba.ac.jp>
2023-10-31 11:28:05 +0900

    add grayscale codes

--- VideoCapture.py
+++ VideoCapture.py
@@ -1,47 +1,45 @@
-"""VideCapture.py
-Author: Shinichi Miyazaki
-
-このプログラムはRaspberry piとカメラで、指定のサイズのビデオを指定のフレームレートと時間で撮影します。
-保存先は USBpath　というところに記載することで指定します。
-"""
-
 import os
 import cv2
 from datetime import datetime
 
-### params ###
+# /dev/video0を指定
+DEV_ID = 0
+
+# params
 Width = 640  # 幅
 Height = 480  # 高さ
 FPS = 10  # フレームレート (frames/sec)
 VideoDuration = 0.5 # ビデオの長さ　(分)
+
 # USBを接続したら、パス名を調べて (右クリックでコピー) 下の""内にペースト
 USBpath = ""
 
 def main():
-    cap = cv2.VideoCapture(0)
-    # 撮影するビデオの設定
+    # /dev/video0を指定
+    cap = cv2.VideoCapture(DEV_ID)
+
+    # パラメータの指定
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, Width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, Height)
     cap.set(cv2.CAP_PROP_FPS, FPS)
 
-    # 保存フォルダの作成
+    # 記録フォルダの作成
     os.makedirs(USBpath + "/Videos", exist_ok=True)
 
-    # 日付を取得してビデオ名の作成
+    # ファイル名に日付を指定
     date = datetime.now().strftime("%Y%m%d_%H%M%S")
-    VideoPath = USBpath + "/Videos" + date + ".mp4"
+    path = USBpath + "/Videos" + date + ".mp4"
 
-    # コーデックの指定
+    # 動画パラメータの指定
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-    out = cv2.VideoWriter(VideoPath, fourcc, FPS, (Width, Height))
+    out = cv2.VideoWriter(path, fourcc, FPS, (Width, Height))
 
     # キャプチャ
     for _ in range(FPS * int(VideoDuration*60)):
         ret, frame = cap.read()
         out.write(frame)
-        # プレビュー表示
+        # 画像表示
         cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1)
 
     # 後片付け
     cap.release()
