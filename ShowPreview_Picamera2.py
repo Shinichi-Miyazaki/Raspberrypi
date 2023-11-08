3be22f0c29d26abf261312ae77b465dd97c4e8b3
Rukaume <s.miyazaki.med@gmail.com>
2023-11-08 20:33:05 +0900

    [add] Picamera2 version

---
+++ ShowPreview_Picamera2.py
@@ -0,0 +1,26 @@
+"""ShowPreview.py
+Author: Shinichi Miyazaki
+
+このプログラムはRaspberry piでカメラのプレビュー画像を表示します。
+Durationという変数に希望の時間を秒単位で記載することで、表示時間を決めることが可能です。
+実行前に、paramsの部分を任意の数値に書き換えてから実行してください。
+"""
+
+from picamera2 import Picamera2, Preview
+import time
+
+### params ###
+Duration = 10 #(sec)
+Resolution = (720, 480) #(x, y)
+def main(Duration):
+    camera = Picamera2()
+    camera_config = camera.create_preview_configuration()
+    camera.configure(camera_config)
+    picam2.start_preview(Preview.QTGL,
+                         width = Resolution[0],
+                         height = Resolution[1])
+    time.sleep(Duration)
+    camera.stop_preview()
+
+if __name__ == '__main__':
+    main(Duration)
---
+++ TimeLapseCapture_Picamera2.py
@@ -0,0 +1,76 @@
+"""TimeLapseCapture.py
+Author: Shinichi Miyazaki
+
+"""
+import datetime
+from picamera2 import Picamera2
+import os
+import time
+import threading
+import numpy as np
+import pandas as pd
+
+### params ###
+interval = 2 #タイムラプスのインターバル (秒)
+num_of_images = 4 #イメージの枚数
+today = datetime.date.today()
+
+# USBを接続したら、パス名を調べて (右クリックでコピー) 下の""内にペースト
+USBpath = ""
+data_dir_path = USBpath + "/TimeLapse{}/".format(today)
+
+# data container
+timelog = []
+
+def take_image_periodically(num):
+    global timelog
+    filename = "{0:05d}".format(num) + ".jpg"
+
+    camera.switch_mode_and_capture_file(capture_config,
+                                        camera.capture(data_dir_path + filename))
+    now = datetime.datetime.now()
+    timelog.append(now.strftime('%H:%M:%S.%f'))
+
+def schedule(interval_sec,
+             callable_task,
+             args=None,
+             kwargs=None):
+    args = args or []
+    kwargs = kwargs or {}
+    # 基準時刻を作る
+    base_timing = datetime.datetime.now()
+    for i in range(num_of_images):
+        # 処理を別スレッドで実行する
+        t = threading.Thread(target=callable_task,
+                             args=(i,),
+                             kwargs=kwargs)
+        t.start()
+
+        # 基準時刻と現在時刻の剰余を元に、次の実行までの時間を計算する
+        current_timing = datetime.datetime.now()
+        elapsed_sec = (current_timing - base_timing).total_seconds()
+        sleep_sec = interval_sec - (elapsed_sec % interval_sec)
+
+        time.sleep(max(sleep_sec, 0))
+
+def main():
+    global timelog
+    global camera
+    global capture_config
+
+    camera = Picamera2()
+    capture_config = camera.create_still_configuration()
+    camera.start(show_preview = True)
+
+    os.makedirs(data_dir_path, exist_ok = True)
+
+    schedule(interval_sec=interval,
+             callable_task=take_image_periodically)
+    timelog = pd.DataFrame(np.array(timelog),
+                           columns=["timelog"])
+    print(timelog)
+    timelog.to_csv(data_dir_path + "/timelog.csv")
+
+
+if __name__ == '__main__':
+    main()
