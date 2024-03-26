a1a4b1afeca8de730334c2686377169a02fee21b
Shinichi <s1930557@s.tsukuba.ac.jp>
2024-03-26 10:21:46 +0900

    bug fix

---
+++ elegans/TimeLapseCapture_for_survival.py
@@ -0,0 +1,83 @@
+"""TimeLapseCapture.py
+Author: Shinichi Miyazaki
+
+"""
+import os
+import datetime
+import time
+import threading
+from picamera2 import Picamera2
+from libcamera import Transform
+
+# パラメータ
+imaging_fps = 1  # タイムラプスのインターバル (秒)
+num_of_images = 30  # イメージの枚数
+imaging_interval = 3 # イメージの撮影間隔 (時間)
+Video_size = (1280, 960) # 動画のサイズ (width, height)
+experiment_name = "survival_test" # 実験名を短い英数字で""の間に記載
+
+# USBを接続したら、パス名を調べて (右クリックでコピー) 下の""内にペースト
+USBpath = "/media/shi/2EEF-F720"
+data_dir_path = USBpath + f"/{experiment_name}/"
+
+# data container
+timelog = []
+
+def take_image_periodically(num):
+    global timelog
+    for i in range(num_of_images):
+        filename = f"Burst_{num:03d}_image_{i:03d}" + ".jpg"
+        camera.switch_mode_and_capture_file(capture_config,
+                                            data_dir_path + filename)
+        time.sleep(1)
+
+    now = datetime.datetime.now()
+    timelog.append(now.strftime('%H:%M:%S.%f'))
+
+
+def schedule(interval_sec,
+             callable_task,
+             args=None,
+             kwargs=None):
+    args = args or []
+    kwargs = kwargs or {}
+    # 基準時刻を作る
+    base_timing = datetime.datetime.now()
+    for i in range(num_of_images):
+        # 処理を別スレッドで実行する
+        t = threading.Thread(target=callable_task,
+                             args=(i,),
+                             kwargs=kwargs)
+        t.start()
+
+        # 基準時刻と現在時刻の剰余を元に、次の実行までの時間を計算する
+        current_timing = datetime.datetime.now()
+        elapsed_sec = (current_timing - base_timing).total_seconds()
+        sleep_sec = interval_sec - (elapsed_sec % interval_sec)
+
+        time.sleep(max(sleep_sec, 0))
+
+
+def main():
+    global timelog
+    global camera
+    global capture_config
+
+    camera = Picamera2()
+    capture_config = camera.create_still_configuration(main={"size": Video_size},
+                                                       transform=Transform(hflip=True,
+                                                                           vflip=True))
+    camera.start(show_preview=True)
+    os.makedirs(data_dir_path, exist_ok=True)
+    os.chdir(data_dir_path)
+    schedule(interval_sec=imaging_fps,
+             callable_task=take_image_periodically)
+    with open("timelog.txt", "w") as f:
+        for i in timelog:
+            f.write(i + "\n")
+
+    print(timelog)
+
+
+if __name__ == '__main__':
+    main()
